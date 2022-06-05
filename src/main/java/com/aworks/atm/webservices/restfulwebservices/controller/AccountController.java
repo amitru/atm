4ba6a9aaa98dc712b654a1b7606a5b3c08e642e4
Amit Ruwali <amit.ruwali@gmail.com>
2022-06-05 20:51:19 +0530

    Update AccountController.java

--- src/main/java/com/aworks/atm/webservices/restfulwebservices/controller/AccountController.java
+++ src/main/java/com/aworks/atm/webservices/restfulwebservices/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.aworks.atm.webservices.restfulwebservices.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +16,21 @@ import com.aworks.atm.webservices.restfulwebservices.handler.AccountsHandler;
 @RestController
 public class AccountController {
 	
+	private static final Logger logger = LogManager.getLogger(AccountController.class);
+	
+	
 	@Autowired
 	AccountsHandler accountsHandler;
 
 	@PostMapping("/get-balance")
 	public CheckBalanceResponse getBalance(@RequestBody Account account) {
+		logger.debug("getBalance() Entered-" + account.getNumber() );
 		return accountsHandler.fetchBalance(account);
 	}
 	
 	@PostMapping("/withdraw-balance")
 	public WithdrawalResponse withdrawBalance(@RequestBody WithdrawlRequest wReq) {
+		logger.debug("getBalance() Entered-" + wReq.getAccount() + " " + wReq.getAmount());
 		return accountsHandler.withdrawBalance(wReq);
 	}
 	
